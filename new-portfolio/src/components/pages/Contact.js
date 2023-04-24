a6e6a14abaa6e0630631a4d23b76dd1c1a739fe5
Jeremiah Dorvil <jeremiahdorvil@outlook.com>
2023-04-24 08:06:01 -0400

    added icons and links to the contact page.

--- new-portfolio/src/components/pages/Contact.js
+++ new-portfolio/src/components/pages/Contact.js
@@ -1,22 +1,34 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { icon } from '@fortawesome/fontawesome-svg-core/import.macro';
 
 export default function Contact() {
   return (
     <div>
       <h1>Contact Page</h1>
       <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
+        Contact me at any of the following links.
       </p>
+      <div>
+        <a href= 'http://www.github.com'>
+        <FontAwesomeIcon icon={icon({name: "github", family: "solid"})} />
+        </a>
+      </div>
+      <div>
+        <a href='http://www.linkedin.com/in/jeremiah-dorvil'>
+        <FontAwesomeIcon icon={icon({name: "linkedin", family: "solid"})} />
+        </a>
+      </div>
+      <div>
+        <a href='mailto:jeremiahdorvil@outlook.com'>
+        <FontAwesomeIcon icon={icon({name: "square-envelope", family: "solid"})} />
+        </a>
+      </div>
+      <div>
+        <a href='tel:4754550981'>
+        <FontAwesomeIcon icon = {icon({name:"mobile-screen-button", family: "solid"})} />
+        </a>
+      </div>
     </div>
   );
 }
