1117baae01b608756d5ddf513600879fd80eab4a
Jeremiah Dorvil <jeremiahdorvil@outlook.com>
2023-04-26 18:18:57 -0400

    made edits to bootstrap

--- new-portfolio/src/components/pages/Contact.js
+++ new-portfolio/src/components/pages/Contact.js
@@ -5,33 +5,41 @@ import { icon } from '@fortawesome/fontawesome-svg-core/import.macro';
 
 export default function Contact() {
   return (
-    <div className='container col-4'>
+    <div className='row align-items-start'>
+    <div className='justify-content-center'>
       <h1>Contact Page</h1>
       <p>
         Contact me at any of the following links.
       </p>
-      <div>
-        <a href= {'http://www.github.com'}>
-        <i class="fa-brands fa-github"></i>
-        {/* <FontAwesomeIcon icon="fa-brands fa-github" />        */}
+      <div className='col'>
+        <a href= {'http://www.github.com/jeremiahdorvil'}>
+        <button type="button" class="btn btn-dark">
+        <i class="fa-brands fa-github fa-lg"></i>
+        </button>
         </a> 
       </div>
-      <div>
+      <div className='col'>
         <a href={'http://www.linkedin.com/in/jeremiah-dorvil'}>
-        <i class="fa-brands fa-linkedin"></i>
-        {/* <FontAwesomeIcon icon="fa-brands fa-linkedin" />  */}
+        <button type="button" class="btn btn-dark">
+        <i class="fa-brands fa-linkedin fa-lg"></i>
+        </button>
         </a> 
       </div>
-      <div>
+      <div className='col'>
         <a href={'mailto:jeremiahdorvil@outlook.com'}>
-        <FontAwesomeIcon icon={icon({name: "square-envelope", family: "classic", style: "solid"})} />
+        <button type="button" class="btn btn-dark">
+        <FontAwesomeIcon icon={icon({name: "square-envelope", family: "classic", style: "solid"})} size = {32} />
+        </button>
         </a>
       </div>
-      <div>
+      <div className='col'>
         <a href={'tel:4754550981'}>
-        <FontAwesomeIcon icon = {icon({name:"mobile-screen-button", family: "classic", style: "solid"})} />
+        <button type="button" class="btn btn-dark">
+        <FontAwesomeIcon icon = {icon({name:"mobile-screen-button", family: "classic", style: "solid"})} size = {32} />
+        </button>
         </a>
       </div>
+    </div>
     </div>
   );
 }
